baabac4a7ab1454049662a89c870185467a7a857
Sek Un <michael@boreddev.com>
2020-04-08 20:59:56 +0800

    Update pool_size

--- lib/phoenix_hello/repo.ex
+++ lib/phoenix_hello/repo.ex
@@ -2,7 +2,7 @@ defmodule PhoenixHello.Repo do
   use Ecto.Repo,
     otp_app: :phoenix_hello,
     adapter: Ecto.Adapters.Postgres,
-    pool_size: 22
+    pool_size: 10
 
     def init(_type, config) do
       {:ok, Keyword.put(config, :url, System.fetch_env!("DATABASE_URL"))}
